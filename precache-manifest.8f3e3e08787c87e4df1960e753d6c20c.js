491bb4c279f236048399561890a2ac6688fbda56
peddisetti saikiran <peddisettisai21@gmail.com>
2021-11-01 11:14:10 +0530

    Updates

---
+++ precache-manifest.8f3e3e08787c87e4df1960e753d6c20c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ef135286ea22ceb896d535ed469d20",
+    "url": "/coding_monks/index.html"
+  },
+  {
+    "revision": "1a450c4f1ba33d2eb4a4",
+    "url": "/coding_monks/static/css/2.f82b39a3.chunk.css"
+  },
+  {
+    "revision": "c7970ef9368ce5cb1058",
+    "url": "/coding_monks/static/css/main.b0aed7c4.chunk.css"
+  },
+  {
+    "revision": "1a450c4f1ba33d2eb4a4",
+    "url": "/coding_monks/static/js/2.22adf320.chunk.js"
+  },
+  {
+    "revision": "1c0976d7ba9e8390a967350ca889a860",
+    "url": "/coding_monks/static/js/2.22adf320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bb8d8c3206fa22b347",
+    "url": "/coding_monks/static/js/3.dde00f4f.chunk.js"
+  },
+  {
+    "revision": "c7970ef9368ce5cb1058",
+    "url": "/coding_monks/static/js/main.ce2cc33e.chunk.js"
+  },
+  {
+    "revision": "cbf9a83b8a22d1a8503b",
+    "url": "/coding_monks/static/js/runtime-main.c3412bc4.js"
+  },
+  {
+    "revision": "6f2afe5c606318a9a79c01409b458fb7",
+    "url": "/coding_monks/static/media/1.6f2afe5c.jpg"
+  },
+  {
+    "revision": "c882b474a3aaa340d00bc17124087729",
+    "url": "/coding_monks/static/media/2.c882b474.jpg"
+  },
+  {
+    "revision": "2d2b96e4be1bbc436c665d2e21b108de",
+    "url": "/coding_monks/static/media/3.2d2b96e4.jpg"
+  },
+  {
+    "revision": "d931fae9858ed108f59a17fa3168e6a0",
+    "url": "/coding_monks/static/media/adobe.d931fae9.jpg"
+  },
+  {
+    "revision": "96c7f8925691c927296ef4c3a6f07420",
+    "url": "/coding_monks/static/media/amazon-9538.96c7f892.svg"
+  },
+  {
+    "revision": "5a71816ef38ac7f7b82048ca6bd3b42a",
+    "url": "/coding_monks/static/media/curefit.5a71816e.png"
+  },
+  {
+    "revision": "ae4adbc8a072cc82d227b8d2341927d3",
+    "url": "/coding_monks/static/media/download.ae4adbc8.jfif"
+  },
+  {
+    "revision": "92eccd039763e842ed6529a34476af29",
+    "url": "/coding_monks/static/media/dsaimg.92eccd03.png"
+  },
+  {
+    "revision": "290e5868c9ddd23e7210de5f5d8000a1",
+    "url": "/coding_monks/static/media/flower.290e5868.jfif"
+  },
+  {
+    "revision": "d8410c7b0e9c5723dc242a5df705fdec",
+    "url": "/coding_monks/static/media/gs.d8410c7b.png"
+  },
+  {
+    "revision": "ead3d5012b3a80188a6832f53433ec4a",
+    "url": "/coding_monks/static/media/homepage.ead3d501.png"
+  },
+  {
+    "revision": "4bcc0c16b85a4b2bae7d5bf5122e0ab7",
+    "url": "/coding_monks/static/media/jp-morgan.4bcc0c16.svg"
+  },
+  {
+    "revision": "48a1d5666a03af3eeab241e5adcf1470",
+    "url": "/coding_monks/static/media/microsoft.48a1d566.svg"
+  },
+  {
+    "revision": "2c773185b4ca69f62b9c8703feb91cee",
+    "url": "/coding_monks/static/media/oracle-6.2c773185.svg"
+  },
+  {
+    "revision": "1bbc2261f4a83b7c155f522b6c8d929e",
+    "url": "/coding_monks/static/media/sample-instructor.1bbc2261.jfif"
+  },
+  {
+    "revision": "7eb5dc580fd1b5659ac0a9dc57643e7f",
+    "url": "/coding_monks/static/media/samsung-9534.7eb5dc58.svg"
+  },
+  {
+    "revision": "a301a495c8948a46799c8105955dc959",
+    "url": "/coding_monks/static/media/webdev.a301a495.jfif"
+  }
+]);
